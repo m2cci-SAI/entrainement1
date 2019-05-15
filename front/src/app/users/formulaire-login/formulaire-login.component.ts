bc05cf5c17e4b33b72337cb3a07d753dccc7bf4c
AYARI <sadok.ayari@sogeti.com>
2019-05-15 10:23:13 +0200

    add a header non router

--- front/src/app/users/formulaire-login/formulaire-login.component.ts
+++ front/src/app/users/formulaire-login/formulaire-login.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-formulaire-login',
   templateUrl: './formulaire-login.component.html',
-  styleUrls: ['./formulaire-login.component.css']
+  styleUrls: ['./formulaire-login.component.css'
+]
 })
 export class FormulaireLoginComponent implements OnInit {
 
