974bcb43d0a1bb7e1d401adf43e2f580d46ec40d
AYARI <sadok.ayari@sogeti.com>
2019-06-04 00:34:30 +0200

    header avec rooting login register football

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormulaireLoginComponent } from './users/formulaire-login/formulaire-login.component';
+import { FormulaireRegisterComponent } from './users/formulaire-register/formulaire-register.component';
+import { FootballComponent } from './football/football.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: FormulaireLoginComponent
+   },
+   {
+     path: 'register',
+     component: FormulaireRegisterComponent
+    },
+    {
+      path: 'football',
+      component: FootballComponent
+     }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FootballComponent } from './football/football.component';
+import { SidenavbarComponent } from './football/sidenavbar/sidenavbar.component';
+import { FormulaireLoginComponent } from './users/formulaire-login/formulaire-login.component';
+import { FormulaireRegisterComponent } from './users/formulaire-register/formulaire-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FootballComponent,
+    SidenavbarComponent,
+    FormulaireLoginComponent,
+    FormulaireRegisterComponent
   ],
   imports: [
     BrowserModule,
